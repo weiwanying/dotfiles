c2aaa11a8cbeb5e6c411a3d4d559b982374bcdf6
SeanXP <iseanxp+code@gmail.com>
2016-03-26 22:58:33 +0800

    update: hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,17 +1,22 @@
 
 -- Hammerspoon --
 -- http://www.hammerspoon.org/ --
+-- http://larryhynes.net/2015/02/switching-from-slate-to-hammerspoon.html
+--
 
 ------------- Sset hyper to ctrl + cmd ---------------
 local hyper = {'ctrl', 'cmd'}
---local hyper = {'ctrl', 'alt', 'cmd'}
-local hyperShift = {'ctrl', 'alt', 'cmd', 'shift'}
+local hyperShift = {'ctrl', 'cmd', 'shift'}
+
+-- make window transitions much snappier
+hs.window.animationDuration = 0
 
 ------------- Reload config after write ---------------
+-- pathwatcher will automatically reload the config whenever the file changes
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", hs.reload):start()
 hs.alert.show("Hammerspoon Config loaded")
 
-------------- Switch to Application ---------------
+------------- Switch to Application ---------------{{{
 -- App shortcuts
 local key2App = {
     c = 'AppCleaner',
@@ -27,36 +32,182 @@ local key2App = {
 }
 
 for key, app in pairs(key2App) do
-    hs.hotkey.bind(hyper, key, function() 
+    hs.hotkey.bind(hyperShift, key, function() 
         hs.application.launchOrFocus(app) 
     end)
 end
+-- }}}
 
-------------- Window Management ---------------
--- hyper + up maximize the current window
-hs.hotkey.bind(hyper, 'up', function()
-    hs.grid.maximizeWindow() 
+------------- Window Management --------------- {{{
+-- hyper d for left one half window
+hs.hotkey.bind(hyper, 'd', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
 end)
 
--- Hints all valid windows
-hs.hotkey.bind(hyper, 'h', function() 
-    hs.hints.windowHints(getAllValidWindows()) 
-end)
-function getAllValidWindows ()
-	local allWindows = hs.window.allWindows()
-	local windows = {}
-	local index = 1
-	for i = 1, #allWindows do
-		local w = allWindows[i]
-		if w:screen() then
-			windows[index] = w
-			index = index + 1
-		end
-	end
-	return windows
-end
+-- hyper g for right one half window
+hs.hotkey.bind(hyper, 'g', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
 
-------------- Multiple Monitor Management ---------------
+        f.x = max.x + (max.w / 2)
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-- hyper f for fullscreen
+hs.hotkey.bind(hyper, 'f', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-- hyper r for top left one quarter window
+hs.hotkey.bind(hyper, 'r', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+
+-- hyper t for top right one quarter window
+hs.hotkey.bind(hyper, 't', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-- hyper v for bottom left one quarter window
+hs.hotkey.bind(hyper, 'v', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y + (max.h / 2)
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+
+-- hyper c for bottom right one quarter window
+hs.hotkey.bind(hyper, 'c', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y + (max.h / 2)
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-- Hyper i to show window hints
+hs.hotkey.bind(hyper, 'i', function() 
+    hs.hints.windowHints() 
+end)
+
+-- Hyper hjkl to switch window focus
+hs.hotkey.bind(hyper, 'k', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowNorth()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyper, 'j', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowSouth()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyper, 'l', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowEast()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyper, 'h', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowWest()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+--}}}
+
+------------- Multiple Monitor Management --------------- {{{
 -- hyper + left move the current window to the left monitor
 hs.hotkey.bind(hyper, 'left', function() 
     local w = hs.window.focusedWindow()
@@ -80,3 +231,5 @@ hs.hotkey.bind(hyper, 'right', function()
         w:moveToScreen(s)
     end
 end)
+
+-- }}}
