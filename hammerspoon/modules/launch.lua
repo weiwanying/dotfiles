60099c5d648a06741f8d56eeae23890e8b7da22c
SeanXP <iseanxp+code@gmail.com>
2016-03-30 08:29:58 +0800

    hammerspoon: toggle application

--- hammerspoon/modules/launch.lua
+++ hammerspoon/modules/launch.lua
@@ -1,11 +1,11 @@
 local hotkey = require 'hs.hotkey'
+local window = require 'hs.window'
 local application = require 'hs.application'
 
 ------------- Switch to Application ---------------{{{
 local key2App = {
     c = 'AppCleaner',
     e = 'Evernote',
-    --h = 'iTerm',
     j = 'Google Chrome',
     m = 'Finder',
     n = 'NeteaseMusic',
@@ -18,7 +18,8 @@ local key2App = {
 
 for key, app in pairs(key2App) do
     hotkey.bind(hyper, key, function() 
-        application.launchOrFocus(app) 
+        --application.launchOrFocus(app) 
+        toggle_application(app)
     end)
 end
 
@@ -27,9 +28,44 @@ end
 
 -- iTerm console
 hotkey.bind(hyper, ';', function()
-    application.launchOrFocus("iTerm")
+    toggle_application("iTerm")
 end)
 
 -- reload
 hotkey.bind(hyper, 'escape', function() hs.reload() end )
+--}}}
+
+-- {{{ toggle_application
+-- Toggle an application between being the frontmost app, and being hidden
+function toggle_application(_app)
+    -- finds a running applications
+	local app = application.find(_app)
+
+	if not app then
+        -- application not running, launch app
+        application.launchOrFocus(_app)
+        return
+	end
+
+    -- application running, toggle hide/unhide
+	local mainwin = app:mainWindow()
+	if mainwin then
+		if true == app:isFrontmost() then
+			mainwin:application():hide()
+		else
+			mainwin:application():activate(true)
+			mainwin:application():unhide()
+			mainwin:focus()
+		end
+    else
+        -- no windows, maybe hide
+        if true == app:hide() then
+            -- focus app
+            application.launchOrFocus(_app)
+        else
+            -- nothing to do
+        end
+	end
+end
+
 -- }}}
