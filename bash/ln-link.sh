9633da5da5173ccf77170511efdbb7f352ea1b51
SeanXP <iseanxp+code@gmail.com>
2016-04-10 16:44:10 +0800

    bash: update

---
+++ bash/ln-link.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#################################################################
+#   Copyright (C) 2016 Sean Guo. All rights reserved.
+#
+#	> File Name:        < ln-link.sh >
+#	> Author:           < Sean Guo >
+#	> Mail:             < iseanxp+code@gmail.com >
+#	> Created Time:     < 2016/04/16 >
+#	> Last Changed:
+#	> Description:
+#################################################################
+
+dotfile="bashrc"
+target="$HOME/.$dotfile"
+
+if [ -f "$target" ];then
+  # backuo old config file
+  mv "$target" "$target".old
+fi
+
+# make soft link
+ln -s `pwd`/$dotfile $target
