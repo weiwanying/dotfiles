5592e56705564e872351c2ebbe7462e71e6d3adb
SeanXP <iseanxp+code@gmail.com>
2016-04-17 21:01:22 +0800

    hammperspoon: Mouse Hightlight

--- hammerspoon/modules/windows.lua
+++ hammerspoon/modules/windows.lua
@@ -176,3 +176,28 @@ hotkey.bind(hyper, 'Down', grid.pushWindowDown)
 --end)
 
 -- }}}
+
+------------- Mouse Hightlight --------------- {{{
+hotkey.bind(hyper, '0', function() mouseHighlight() end)
+-- draw a bright red circle around the mouse pointer for a few seconds
+function mouseHighlight()
+    if mouseCircle then
+        mouseCircle:delete()
+        if mouseCircleTimer then
+            mouseCircleTimer:stop()
+        end
+    end
+    mousepoint = hs.mouse.getAbsolutePosition()
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+    mouseCircle:setFill(false)
+    mouseCircle:setStrokeWidth(5)
+    mouseCircle:bringToFront(true)
+    mouseCircle:show(0.5)
+
+    mouseCircleTimer = hs.timer.doAfter(3, function()
+        mouseCircle:hide(0.5)
+        hs.timer.doAfter(0.6, function() mouseCircle:delete() end)
+    end)
+end
+--}}}
