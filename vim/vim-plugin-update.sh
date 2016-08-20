2af31b82154bb9dc5c8dfbcd32bcde4ff5155750
SeanXP <iseanxp+code@gmail.com>
2016-08-20 08:06:37 +0800

    vim script

---
+++ vim/vim-plugin-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#################################################################
+#   Copyright (C) 2016 Sean Guo. All rights reserved.
+#
+#   > File Name:        < vim-plugin-update.sh >
+#   > Author:           < Sean Guo >
+#   > Mail:             < iseanxp+code@gmail.com >
+#   > Created Time:     < 2016/04/17 >
+#   > Description:
+#################################################################
+vim +PluginUpdate +qall &
