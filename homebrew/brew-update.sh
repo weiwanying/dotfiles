3059ccc74fa74705d07fab7d032f260d2e517a33
SeanXP <iseanxp+code@gmail.com>
2016-08-29 07:31:05 +0800

    homebrew: update script

--- homebrew/brew-update.sh
+++ homebrew/brew-update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #################################################################
 #   Copyright (C) 2016 Sean Guo. All rights reserved.
-#														  
+#
 #	> File Name:        < brew-update.sh >
-#	> Author:           < Sean Guo >		
-#	> Mail:             < iseanxp+code@gmail.com >		
+#	> Author:           < Sean Guo >
+#	> Mail:             < iseanxp+code@gmail.com >
 #	> Created Time:     < 2016/03/28 >
-#	> Last Changed: 
+#	> Last Changed:
 #	> Description:
 #################################################################
 
@@ -14,7 +14,7 @@ brew="/usr/local/bin/brew"
 
 $brew update
 $brew upgrade --cleanup
-#$brew cleanup
+$brew cleanup
 
 $brew cask update
 $brew cask cleanup
