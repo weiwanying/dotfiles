9fbeb4ff72b04043a51e4f17424b7db5831014fa
SeanXP <iseanxp+code@gmail.com>
2017-02-21 08:27:03 +0800

    OpenWrt: update dnsmasq script

--- OpenWrt/shadowsocks/get_dnsmasq-china-list.sh
+++ OpenWrt/shadowsocks/get_dnsmasq-china-list.sh
@@ -6,10 +6,12 @@
 #   > Author:           < Sean Guo >
 #   > Mail:             < iseanxp+code@gmail.com >
 #   > Created Time:     < 2017/02/17 >
-#   > Description:
+#   > Description:      https://github.com/felixonmars/dnsmasq-china-list
 #################################################################
 
 mkdir -p /etc/dnsmasq.d/ && mv /etc/dnsmasq.d/
+echo "conf-dir=/etc/dnsmasq.d" >> /etc/dnsmasq.conf
 wget --no-check-certificate https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
 wget --no-check-certificate https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/bogus-nxdomain.china.conf
 wget --no-check-certificate https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf
+/etc/init.d/dnsmasq restart
